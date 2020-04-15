201309682a1df1f3c772069e1dcb8de656e02817
Radames Pereira <radamesp@gmail.com>
2020-04-15 01:02:33 -0300

    quarto

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,6 +45,28 @@ const IndexPage = () => {
       }
 
       const { data = [] } = response;
+
+const hasData = Array.isArray(data) && data.length > 0;
+
+if ( !hasData ) return;
+
+const geoJson = {
+  type: 'FeatureCollection',
+  features: data.map((country = {}) => {
+    const { countryInfo = {} } = country;
+    const { lat, long: lng } = countryInfo;
+    return {
+      type: 'Feature',
+      properties: {
+       ...country,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [ lng, lat ]
+      }
+    }
+  })
+}
   }
 
     
